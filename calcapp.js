ce65e753ebc94867e376d4a4a3a6bae2eff15d35
huong9x <huongnv@sphinx-software.com>
2019-03-21 10:18:24 +0700

    an OOP Calculator javascript

---
+++ calcapp.js
@@ -0,0 +1,30 @@
+
+function plus(x, y) {
+	return x + y
+};
+
+function minus(x, y) {
+	return x - y
+};
+
+function mult(x, y) {
+	return x * y
+};
+
+function divi(x, y) {
+	if (y == 0) {
+		return "Infinity Error";
+	} else
+		return x / y;
+};
+
+var operations = {
+	'+': plus,
+	'-': minus,
+	'*': mult,
+	'/': divi
+};
+var calculate = function (x, y, operation) {
+	return operations[operation](x, y);
+}
+console.log(calculate(20, 5, '/'));
