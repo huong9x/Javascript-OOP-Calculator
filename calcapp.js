3b0866e30a799170b143a6dd8add65b1fe354b2f
huong9x <huongnv@sphinx-software.com>
2019-03-21 10:29:48 +0700

    edit some function name

--- calcapp.js
+++ calcapp.js
@@ -1,17 +1,17 @@
 
-function plus(x, y) {
+function add(x, y) {
 	return x + y
 };
 
-function minus(x, y) {
+function sub(x, y) {
 	return x - y
 };
 
-function mult(x, y) {
+function multiply(x, y) {
 	return x * y
 };
 
-function divi(x, y) {
+function divided(x, y) {
 	if (y == 0) {
 		return "Infinity Error";
 	} else
@@ -19,10 +19,10 @@ function divi(x, y) {
 };
 
 var operations = {
-	'+': plus,
-	'-': minus,
-	'*': mult,
-	'/': divi
+	'+': add,
+	'-': sub,
+	'*': multiply,
+	'/': divided
 };
 var calculate = function (x, y, operation) {
 	return operations[operation](x, y);
