ee6a9da6086dcc85a77f74b9cb140d17ee52e151
Huong Nguyen <huongnguye2@my.smccd.edu>
2019-03-21 11:26:05 +0700

    Add files via upload

--- calcapp.js
+++ calcapp.js
@@ -1,31 +1,37 @@
 
-function add(x, y) {
-	return x + y
-};
+class Calculate {
+	constructor() {
+		this.operations = {
+			'+': add,
+			'-': sub,
+			'*': multiply,
+			'/': divided
+		}
+	}
 
-function sub(x, y) {
-	return x - y
-};
+	add(x, y) {
+		return x + y
+	}
 
-function multiply(x, y) {
-	return x * y
-};
+	sub(x, y) {
+		return x - y
+	}
 
-function divided(x, y) {
-	if (y == 0) {
-		return "Infinity Error";
-	} else
-		return x / y;
-};
+	multiply(x, y) {
+		return x * y
+	}
 
-var operations = {
-	'+': add,
-	'-': sub,
-	'*': multiply,
-	'/': divided
-};
-var calculate = function (x, y, operation) {
-	return operations[operation](x, y);
+	divided(x, y) {
+		if (y == 0) {
+			return "Infinity Error";
+		} else
+			return x / y;
+	}
+
+	do(x, y, operation) {
+		return operations[operation](x, y);
+	}
 }
 
-console.log(calculate(20, 5, '/'));
+let cal = new Calculate()
+console.log(cal.do(20, 5, '/'));
